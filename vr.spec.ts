b1cca71f09bc5f870a329a1bb6746dbce0c0912b
Vaibhav Tapariya <132330551+vaibhav7899ops@users.noreply.github.com>
2025-11-09 13:50:05 +0530

    Delete vr.spec.ts

--- vr.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('pulse table visual regression', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForSelector('text=results');
-  await expect(page).toHaveScreenshot('pulse-table.png', { maxDiffPixels: 2 });
-});
